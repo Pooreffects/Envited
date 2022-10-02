c8f39cb384a1998ed65c50cbbd71d2200367d393
Mohamed Benabdellah <pooreffects@gmail.com>
2022-10-02 23:03:38 +0100

    adjust: home redirection animation

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,13 +1,17 @@
 import Link from 'next/link';
-import React from 'react';
-
+import { motion } from 'framer-motion';
 export default function Navbar() {
   return (
     <div className="p-6">
       <Link href="/">
-        <a className="rounded-xl text-lg text-white font-semibold bg-heading p-2 hover:bg-opacity-90 hover:cursor-pointer">
+        <motion.a
+          initial={{ opacity: 0, x: -30 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3, ease: 'easeInOut' }}
+          className="rounded-xl text-lg text-white font-semibold bg-heading p-2 hover:bg-opacity-90 hover:cursor-pointer"
+        >
           Home
-        </a>
+        </motion.a>
       </Link>
     </div>
   );
